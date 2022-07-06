dca080b0ef2830e4f94106da9eb2360bebfaa724
EuleHaken <sven220ce@gmail.com>
2022-07-06 20:00:31 +0200

    added PrivmsgMessage - representation of a `PRIVMSG` irc messaage

---
+++ src/PrivmsgMessage.cpp
@@ -0,0 +1,28 @@
+#include "PrivmsgMessage.hpp"
+
+WEBSOCKET_BEGIN_NAMESPACE
+
+PrivmsgMessage::PrivmsgMessage(IrcMessage& parent)
+    : IrcMessage(parent)
+{
+    this->_username = parent.getPrefix().split('!').constFirst();
+    this->_channel = parent.getParams().constLast();
+    this->_message = parent.getParams().constLast();
+}
+
+const QString& PrivmsgMessage::getUsername() const
+{
+    return this->_username;
+}
+
+const QString& PrivmsgMessage::getChannel() const
+{
+    return this->_channel;
+}
+
+const QString& PrivmsgMessage::getMessage() const
+{
+    return this->_message;
+}
+
+WEBSOCKET_END_NAMESPACE
