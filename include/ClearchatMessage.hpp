59079dc420b7e1b52e048a1099d5acd6b4216e8a
EuleHaken <sven220ce@gmail.com>
2022-07-08 11:18:37 +0200

    implemented methods and fields for ClearchatMessage

--- include/ClearchatMessage.hpp
+++ include/ClearchatMessage.hpp
@@ -10,6 +10,13 @@ class WEBSOCKET_EXPORT ClearchatMessage final : public IrcMessage
 public:
     ClearchatMessage(IrcMessage& parent);
 
+public:
+    const QString& getChannel() const;
+    //const QString& getUser() const;
+    const QString& getTarget() const;
+    const bool isPermaban() const;
+    const int getLength() const;
+
 private:
     /**
      * @brief The channel where the `CLEARCHAT` was raised
@@ -17,10 +24,28 @@ private:
     QString _channel;
 
     /**
-     * @brief The user
+     * @brief The user who raised the `CLEARCHAT`
+     *
+     * Note: Actually not possible ATM :(
      */
-    QString _user;
+    //QString _user;
 
+    /**
+     * @brief The target for the `CLEARCHAT`
+     * For example, /ban {target}
+     */
+    QString _target;
+
+    /**
+     * @brief Wheter the `CLEARCHAT` was a permaban or not
+     */
+    bool _isPermaban{false};
+
+    /**
+     * @brief The length, for how long the `CLEARCHAT` should go on
+     * -1 = permaban
+     */
+    int _length{-1};
 };
 
 WEBSOCKET_END_NAMESPACE
--- src/ClearchatMessage.cpp
+++ src/ClearchatMessage.cpp
@@ -2,4 +2,52 @@
 
 WEBSOCKET_BEGIN_NAMESPACE
 
+ClearchatMessage::ClearchatMessage(IrcMessage& parent)
+    : IrcMessage(parent)
+{
+    assert(parent.getCommand() == "CLEARCHAT");
+
+    this->_channel = parent.getParams().constFirst();
+    this->_target = parent.getParams().constLast();
+    this->_length = parent.getTags().value("ban-duration", "").toInt();
+
+    bool isOk = false;
+    int length = parent.getTags().value("ban-duration", "").toInt(&isOk);
+    if (isOk)
+    {
+        this->_length = length;
+    }
+    else
+        this->_length = -1;
+
+    this->_isPermaban = this->_length == -1;
+}
+
+const QString& ClearchatMessage::getChannel() const
+{
+    return this->_channel;
+}
+
+/*
+const QString& ClearchatMessage::getUser() const
+{
+    return this->_user;
+}
+*/
+
+const QString& ClearchatMessage::getTarget() const
+{
+    return this->_target;
+}
+
+const bool ClearchatMessage::isPermaban() const
+{
+    return this->_isPermaban && this->_length == -1;
+}
+
+const int ClearchatMessage::getLength() const
+{
+    return this->_length;
+}
+
 WEBSOCKET_END_NAMESPACE
