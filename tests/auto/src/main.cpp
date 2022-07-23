b23aab9f534c2ae04f651c7af46de5ed0b876de5
EuleHaken <sven220ce@gmail.com>
2022-07-23 13:19:26 +0200

    removed temp/unnecessary code

--- tests/auto/src/main.cpp
+++ tests/auto/src/main.cpp
@@ -3,10 +3,6 @@
 
 int main(int argc, char *argv[])
 {
-    bool forsen = true;
-
-    forsen ? void(qInfo() << "forsen is true") : void();
-
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
